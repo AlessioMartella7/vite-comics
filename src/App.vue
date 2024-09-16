8ced5e997a09de968a97e08c0060f55a329e7888
AlessioMartella7 <alessio.mart87@gmail.com>
2024-09-16 15:43:56 +0200

    added components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
-<script setup>
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooterTop from "./components/AppFooterTop.vue"
+import AppFooterBottom from "./components/AppFooterBottom.vue";
 
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components:{
+    AppHeader,
+    AppMain,
+    AppFooterTop,
+    AppFooterBottom
+  }
+
+}
 </script>
 
 <template>
-
+<AppHeader/>
+<AppMain/>
+<AppFooterTop/>
+<AppFooterBottom/>
 </template>
 
 <style scoped>
---
+++ src/components/AppFooterBottom.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+<div>FOOTER</div>
+</template>
+
+<style scoped>
+</style>
