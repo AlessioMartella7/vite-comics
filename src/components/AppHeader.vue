51c2ed5cb21eb86bd904f4b03546e20c5050ed43
AlessioMartella7 <alessio.mart87@gmail.com>
2024-09-16 16:17:47 +0200

    added header setup

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,10 +9,48 @@ export default {
 </script>
 
 <template>
-<div>
-    HEADER
-</div>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="../assets/img/dc-logo.png" alt="logo-dc-sm">
+            </div>
+
+            <div class="navbar">
+                <nav>
+                    <ul>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </header>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+
+header {
+    background-color: #fff;
+    padding: 1.5rem;
+    margin-top: 1.5rem;
+}
+
+.container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+ul {
+    display: flex;
+    gap: 2rem;
+}
 </style>
