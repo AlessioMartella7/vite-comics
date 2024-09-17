ec5e7e12d54513e08510ed193bedf39144cd9b7f
AlessioMartella7 <alessio.mart87@gmail.com>
2024-09-17 16:11:46 +0200

    added props to AppMainCardList

---
+++ src/components/AppMainCardList.vue
@@ -0,0 +1,37 @@
+<script>
+export default {
+  data() {
+    return {
+      
+    }
+  },
+  props: {
+    cardImage: {
+        type: String,
+        required: true,
+    },
+    cardPrice: {
+        type: String,
+        required: false,
+    },
+    cardSeries: {
+        type: String,
+        required: true,
+    },
+    cardType: {
+        type: String,
+        required: false,
+    },
+  }
+}
+</script>
+
+<template>
+<div class="card">
+    <img :src="cardImage" :alt="cardSeries">
+</div>
+<h3>{{ cardSeries }}</h3>
+</template>
+
+<style scoped>
+</style>
